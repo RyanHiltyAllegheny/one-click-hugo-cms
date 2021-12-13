d9428ba3a94fe7fecb52f46b29afd45722c1984d
RyanHiltyAllegheny <42868768+RyanHiltyAllegheny@users.noreply.github.com>
2021-12-13 17:20:42 -0500

    Murphy

--- src/js/cms-preview-templates/home.js
+++ src/js/cms-preview-templates/home.js
@@ -52,7 +52,7 @@ export default class PostPreview extends React.Component {
               </div>
 
               <div className="w-60-l ph2-l">
-                <img src="img/Murphy.jpeg" alt="" className="mb3"/>
+                <img src="img/Murphy.JPG" alt="" className="mb3"/>
               </div>
             </div>
 
